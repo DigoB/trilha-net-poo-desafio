a7d814f7166140ee1b853c9e94ee765963ed014e
Rodrigo Braz <braz.rodrigo94@gmail.com>
2023-12-14 15:59:08 -0300

    feat: Criada a implementacao da abstracao do celular

--- Program.cs
+++ Program.cs
@@ -1,3 +1,22 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nokia = new Nokia("11111111111", "Nokia A", "22222", 64);
+
+Console.WriteLine("================== Testes Nokia =====================");
+Console.WriteLine();
+
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("GitHub");
+
+Console.WriteLine();
+Console.WriteLine("=====================================================");
+Console.WriteLine();
+
+Console.WriteLine("================== Testes Iphone ====================");
+
+Iphone iphone = new Iphone("22222222222", "Iphone B", "33333", 128);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("DIO");
